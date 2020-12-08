8a894da5eeddd74a6205e2767c9ccef52a8dfb37
wenbin-hu <chesterhu0801@gmail.com>
2020-12-08 18:48:57 +0000

    Update the MATLAB analysis files.

--- src/python_controller/k_means_state_input.m
+++ src/python_controller/k_means_state_input.m
@@ -17,3 +17,16 @@ dist_between_cluster_3(:,:,4) = func_k_means_state(3, 'f,dq,df');
 %% get the sum of distances between clusters
 sum_dist_4 = sum(dist_between_cluster_4, 2);
 sum_dist_3 = sum(dist_between_cluster_3, 2);
+
+%% get the average of distances between clusters
+mean_dist_4 = zeros(size(dist_between_cluster_4, 1), 1);
+for i = 1:size(mean_dist_4, 1)
+    mean_dist_4(i) = sum_dist_4(i) / (i+1);
+end
+
+mean_dist_3 = zeros(size(sum_dist_3));
+for i = 1:size(mean_dist_3, 3)
+    for j = 1:size(mean_dist_3, 1)
+        mean_dist_3(j,1,i) = sum_dist_3(j,1,i) / (j+1);
+    end
+end
